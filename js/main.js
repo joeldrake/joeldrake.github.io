fd6bd7355197f618d2fe225be11e53d5e66fa6f7
Joel Drake <joel@infoping.se>
2018-12-14 15:17:21 +0100

    Added player

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ if ('serviceWorker' in navigator && window.location.hostname !== 'localhost') {
     .register('/serviceWorker.js')
     .catch(err => console.error('Service worker registration failed', err));
 }
+
+const spotifyPlayer = document.getElementById('spotify');
+
+function togglePlayer() {
+  spotifyPlayer.classList.toggle('open');
+}
