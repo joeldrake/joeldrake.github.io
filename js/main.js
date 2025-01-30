eefdf4ddd1037f62fc28eeee10fceedf5d535d8a
Joel Drake <joeldrak3@outlook.com>
2025-01-30 15:39:59 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -1,30 +1,5 @@
-if ('serviceWorker' in navigator && window.location.hostname !== 'localhost') {
+if ("serviceWorker" in navigator && window.location.hostname !== "localhost") {
   navigator.serviceWorker
-    .register('/serviceWorker.js')
-    .catch(err => console.error('Service worker registration failed', err));
+    .register("/serviceWorker.js")
+    .catch((err) => console.error("Service worker registration failed", err));
 }
-
-const spotifyPlayer = document.getElementById('spotify');
-const megamanDiv = document.getElementById('megaman');
-
-function togglePlayer() {
-  spotifyPlayer.classList.toggle('open');
-}
-
-let x = 0;
-let y = 0;
-setInterval(() => {
-  let top = 15 - y * 100;
-  let left = 15 - x * 100;
-
-  megamanDiv.style.backgroundPosition = `${left}px ${top}px`;
-
-  x++;
-
-  if (x === 5) {
-    y++;
-    x = 0;
-  }
-
-  if (y === 3) y = 1;
-}, 300);
