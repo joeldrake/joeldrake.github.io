d2073f2a7cd133071d43ba98783cbf036ddc8610
Joel Drake <joeldrak3@outlook.com>
2025-02-04 12:40:43 +0100

    service worker for log

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,53 +1,53 @@
-const CUSTOM_CACHE = 'custom-cache-v1';
+const CUSTOM_CACHE = "custom-cache-v1";
 
 // A list of local resources we always want to be cached.
-const PRECACHE_URLS = ['./'];
+const PRECACHE_URLS = ["/", "/log/"];
 
 // The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches
       .open(CUSTOM_CACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting()),
+      .then((cache) => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   const currentCaches = [CUSTOM_CACHE];
   event.waitUntil(
     caches
       .keys()
-      .then(cacheNames => {
+      .then((cacheNames) => {
         return cacheNames.filter(
-          cacheName => !currentCaches.includes(cacheName),
+          (cacheName) => !currentCaches.includes(cacheName)
         );
       })
-      .then(cachesToDelete => {
+      .then((cachesToDelete) => {
         return Promise.all(
-          cachesToDelete.map(cacheToDelete => {
+          cachesToDelete.map((cacheToDelete) => {
             return caches.delete(cacheToDelete);
-          }),
+          })
         );
       })
-      .then(() => self.clients.claim()),
+      .then(() => self.clients.claim())
   );
 });
 
 //network first
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     //network first fetch, on error check cache
     fetch(event.request)
-      .then(response => {
+      .then((response) => {
         return response;
       })
       .catch(() => {
-        return caches.open(CUSTOM_CACHE).then(cache => {
-          return cache.match(event.request).then(matching => {
-            return matching || Promise.reject('no-match');
+        return caches.open(CUSTOM_CACHE).then((cache) => {
+          return cache.match(event.request).then((matching) => {
+            return matching || Promise.reject("no-match");
           });
         });
-      }),
+      })
   );
 });
